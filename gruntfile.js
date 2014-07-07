492755cfa9f0784d9d4f782dd01996505a542cdd
Shaun Bulbrook <shaun@jafarstudios.com>
2014-07-07 20:39:18 +0100

    init

---
+++ gruntfile.js
@@ -0,0 +1,7 @@
+module.exports = function(grunt) {
+
+	//Build me my LESS
+	grunt.registerTask('default', 'Style me up bro!', funtion() {
+		grunt.log.write('OK Bro! Let me fetch your hat!.....').ok();
+	});
+}
